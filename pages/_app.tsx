781cb69c1b5c9a622e1244d766f415c71d07d19c
wtfpeanut <p-do-santos@hotmail.com>
2021-02-24 17:20:03 -0300

    add desafios

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,15 @@
 import '../src/styles/global.css';
 
+import { ChallengesProvider } from '../src/contexts/ChallengesContext'
+
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  
+  return (
+    <ChallengesProvider>
+      <Component {...pageProps} />
+    </ChallengesProvider>
+  )
 }
 
 export default MyApp
